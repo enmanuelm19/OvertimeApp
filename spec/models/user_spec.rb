8caffafd9570920af5478e5f28e92a44829ff95c
Enmanuel Medina <enmanuelm19@gmail.com>
2017-01-07 21:27:19 -0430

    Adding custom method in user model plus test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(email: "test@test.com", password: "12341234", password_confirmation: "12341234", first_name: "john", last_name: "snow")
+  end
   describe "creation" do
-    before do
-      @user = User.create(email: "test@test.com", password: "12341234", password_confirmation: "12341234", first_name: "john", last_name: "snow")
-    end
     it "can be created" do
       expect(@user).to be_valid
     end
     it "cannot be created" do
       @user.first_name = nil
       @user.last_name = nil
-      expect(@user).to_not be_valid 
+      expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name method" do
+    it "has a full name method that combine first and last name" do
+      expect(@user.full_name).to eq("Snow, John") 
     end
   end
 end
