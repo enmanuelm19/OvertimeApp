47f1e81ea4f2a49dd701fbb5792c628e5fed61ec
Enmanuel Medina <enmanuelm19@gmail.com>
2017-01-20 20:11:18 -0430

    Added radio_button for status plus test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-@user = User.create(email: "test@test.com", password: "12345678", password_confirmation: "12345678", first_name: "John", last_name: "Snow")
-
+@user = User.create(email: "user@test.com", password: "12345678", password_confirmation: "12345678", first_name: "John", last_name: "Snow")
+AdminUser.create(email: "admin@admin.com", password: "12345678", password_confirmation: "12345678", first_name: "Admin", last_name: "Admin")
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
