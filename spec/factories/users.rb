891ea56db9fdb74e3fcf785b12dc8acee3335bbc
Enmanuel Medina <enmanuelm19@gmail.com>
2017-03-03 18:41:11 -0400

    Added phone validations to user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,7 +10,7 @@ FactoryGirl.define do
     email { generate :email }
     password "12345678"
     password_confirmation "12345678"
-    phone "+584145607888"
+    phone "4145607888"
   end
 
   factory :admin, class: "AdminUser" do
@@ -19,7 +19,7 @@ FactoryGirl.define do
     email "admin@test.com"
     password "12345678"
     password_confirmation "12345678"
-    phone "+584145607888"
+    phone "4145607888"
   end
 
   factory :non_authorized_user, class: "User" do
@@ -28,6 +28,6 @@ FactoryGirl.define do
     email { generate :email }
     password "12345678"
     password_confirmation "12345678"
-    phone "+584145607888"
+    phone "4145607888"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = FactoryGirl.create(:user)
-  end
-  describe "creation" do
-    it "can be created" do
-      expect(@user).to be_valid
+    before do
+        @user = FactoryGirl.create(:user)
     end
-    it "cannot be created without first_name" do
-      @user.first_name = nil
-      expect(@user).to_not be_valid
+    describe 'creation' do
+        it 'can be created' do
+            expect(@user).to be_valid
+        end
     end
-    it "cannot be created without last_name" do
-      @user.last_name = nil
-      expect(@user).to_not be_valid
-    end
-    it "cannot be created without phone" do
-      @user.phone = nil
-      expect(@user).to_not be_valid
-    end
-  end
 
-  describe "custom name method" do
-    it "has a full name method that combine first and last name" do
-      expect(@user.full_name).to eq("Snow, John")
+    describe 'validations' do
+        it 'cannot be created without first_name' do
+            @user.first_name = nil
+            expect(@user).to_not be_valid
+        end
+        it 'cannot be created without last_name' do
+            @user.last_name = nil
+            expect(@user).to_not be_valid
+        end
+        it 'cannot be created without phone' do
+            @user.phone = nil
+            expect(@user).to_not be_valid
+        end
+        it "requires the phone attr to only contain integers" do
+          @user.phone = 'mygreatest'
+          expect(@user).to_not be_valid
+        end
+        it "requires the phone to only have 10 characters" do
+          @user.phone = "123456789012"
+          expect(@user).to_not be_valid 
+        end
     end
-  end
+    describe 'custom name method' do
+        it 'has a full name method that combine first and last name' do
+            expect(@user.full_name).to eq('Snow, John')
+        end
+    end
 end
