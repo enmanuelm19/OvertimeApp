59151eb16b6d0446beff5caa46afa766264794a7
Enmanuel Medina <enmanuelm19@gmail.com>
2017-04-14 16:16:30 -0400

    Blocked non admin users from accesing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -16,8 +16,13 @@ describe 'AuditLog Feature' do
         visit audit_logs_path
         expect(page).to have_content(/Snow, John/)
     end
-    xit 'can not be access for non admin users' do
-        
+    it 'can not be access for non admin users' do
+        logout(:user)
+        user = FactoryGirl.create(:user)
+        login_as(user, :scope => :user)
+
+        visit audit_logs_path
+        expect(current_path).to eq(root_path)
     end
   end
 end
