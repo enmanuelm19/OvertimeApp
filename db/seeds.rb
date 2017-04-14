4b7d3e42a99b29c01989c52fa1bcc1e219f95f9d
Enmanuel Medina <enmanuelm19@gmail.com>
2017-04-14 15:11:45 -0400

    Adding policy for tab audit log

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-@user = User.create(email: "user@test.com",
+@user = User.create!(email: "user@test.com",
                     password: "12345678",
                     password_confirmation: "12345678",
                     first_name: "John",
                     last_name: "Snow",
-                    phone: "+584145607888")
+                    phone: "584145607888")
 
-AdminUser.create(email: "admin@admin.com",
+AdminUser.create!(email: "admin@admin.com",
                   password: "12345678",
                   password_confirmation: "12345678",
                   first_name: "Admin",
                   last_name: "Admin",
-                  phone: "+584145607888")
+                  phone: "584145607888")
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id)
 end
 puts "A 100 posts has been created"
