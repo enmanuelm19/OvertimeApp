4bc731fda27ecadd4436f7d88bce4470f3a13c72
Enmanuel Medina <enmanuelm19@gmail.com>
2017-04-18 21:16:11 -0400

    Fixing seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,13 @@ AdminUser.create!(email: "admin@admin.com",
                   first_name: "Admin",
                   last_name: "Admin",
                   phone: "584145607888")
+AuditLog.create!(user_id: @user.id, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, start_date: (Date.today - 20.days))
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
 end
 
-#AuditLog.create!(user_id: @user.id, start_date: (Date.today - 6.days))
-#AuditLog.create!(user_id: @user.id, start_date: (Date.today - 13.days))
-#AuditLog.create!(user_id: @user.id, start_date: (Date.today - 20.days))
+
 puts "A 100 posts has been created"
